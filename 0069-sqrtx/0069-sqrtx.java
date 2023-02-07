0f87816113c343595e02eceacbfec72696a78bb6
Jeong_Hyun <105183327+jsjhyun@users.noreply.github.com>
2023-02-07 17:18:16 +0900

    Time: 1 ms (100.00%), Space: 39.5 MB (77.07%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -4,7 +4,7 @@ class Solution{
     public int mySqrt(int x) {
        // int ans = 0;
        // double result = sqrt(x);    
-        int ans = (int) sqrt(x);
+        int ans = (int) sqrt(x); // 정수 제곱근을 뽑기 위해 int
         
         return ans;
     }
