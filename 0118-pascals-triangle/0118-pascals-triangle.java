2caadb856244dec38d2aae33c964b8a86605e6c1
Jeong_Hyun <105183327+jsjhyun@users.noreply.github.com>
2023-02-08 11:45:59 +0900

    Time: 1 ms (75.10%), Space: 40.8 MB (50.33%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,7 +3,7 @@ class Solution {
 	    List<List<Integer>> result = new ArrayList<>(); // 2중 리스트를 만들어 공간 생성 
 			
 		for(int i=0; i<numRows; i++) {		// numRows 만큼 List 생성
-			ArrayList<Integer> row = new ArrayList<>(i+1);
+			ArrayList<Integer> row = new ArrayList<>(i+1); // 행이 내려갈수록 하나씩 추가
 
 			if(i == 0 ) {	
 				row.add(1); // 1번째 리스트 요소는 1               
@@ -14,10 +14,10 @@ class Solution {
 					ArrayList<Integer> before = (ArrayList<Integer>) result.get(i-1); 
 					
 					if(j == 0 || j == result.size() ) {	
-                 	       // 행의 첫번째 값과 마지막 값은 1이다.
+                 	       // 행의 첫번째 값과 마지막 값은 1
 						row.add(1);
 					}else {
-                  	      // 전 ArrayList의 자신의 위치-1, 위치 값을 가져와서 더해준다.
+                  	      // 전 ArrayList의 자신의 위치-1, 위치 값을 가져와서 더한다.
 						row.add(before.get(j-1) + before.get(j)); 
 					}
 				}
